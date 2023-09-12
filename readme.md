dcda659b1b3de481abf8a27f29f13badca72b472
choikkk <sixballoons@naver.com>
2023-09-12 13:39:15 +0900

    230912

--- readme.md
+++ readme.md
@@ -1,14 +1,14 @@
 ## 계산기 만들기 해설
 
 ### 1주차
-- [ ] 기본 UI 완성
-- [ ] data 속성 추가
-- [ ] calculator class 만들기
-- [ ] 계산기 기능 관련 DOM 셋팅
-- [ ] 숫자 버튼 기능{onPressNumber} 구현
+- [x] 기본 UI 완성
+- [x] data 속성 추가
+- [x] calculator class 만들기
+- [x] 계산기 기능 관련 DOM 셋팅
+- [x] 숫자 버튼 기능{onPressNumber} 출력 구현
+- [x] 사칙연산 버튼 기능{onPressOperation} 출력 구현
 
 ### 2주차
-- [ ] 사칙연산 버튼 기능{onPressOperation} 구현
 - [ ] onEqual 기능 구현
 - [ ] onReset 기능 구현
 - [ ] onDelete 기능 구현
--- script.js
+++ script.js
@@ -1,13 +1,66 @@
-class Calculator (
+class Calculator {
+    $previousPreview
+    $currentPreview
 
-)
+    constructor($previousPreview, $currentPreview) {
+        this.$previousPreview = $previousPreview
+        this.$currentPreview = $currentPreview
+    }
 
-const cal = new Calculator{}
+    onPressNumber(number) {
+       // TODO: 벨리데이션
+       this.$currentPreview.textContent += number
+    }
 
-const $previousPreview =  document.querySelector{"[data-previous-preview]"}
+    onPressOperation(operation) {
+        this.$previousPreview.textContent = `${this.$currentPreview.textContent} ${operation}`
+        this.$currentPreview.textContent = ''
+    }
+}
 
-// function Calculator {} {
+// 값 표시
+const $previousPreview = document.querySelector('[data-previous-preview]')
+const $currentPreview = document.querySelector('[data-current-preview]')
 
-// }
+// 사칙연산
+const $plus = document.querySelector('[data-btn-plus]')
+const $minus = document.querySelector('[data-btn-minus]')
+const $multiply = document.querySelector('[data-btn-multiply]')
+const $divide = document.querySelector('[data-btn-divide]')
+const $eqaul = document.querySelectorAll('[data-btn-eqaul]')
 
-// const cal = Calculator()
+// 숫자, 연산
+const $numbers = document.querySelectorAll('[data-btn-number]')
+const $operations = document.querySelectorAll('[data-btn-operation]')
+
+const cal = new Calculator($previousPreview, $currentPreview)
+
+$numbers.forEach(($number) => {
+    $number.addEventListener("click", (e) => {
+        cal.onPressNumber(e.target.textContent)
+    })
+})
+
+$operations.forEach(($operation) => {
+    $operation.addEventListener("click", (e) => {
+       switch ($operation) {
+            case $plus:
+                cal.onPressOperation("+")
+                break;
+            case $minus:
+                cal.onPressOperation("-")
+                break;
+            case $multiply:
+                cal.onPressOperation("*")
+                break;
+            case $divide:
+                cal.onPressOperation("÷")
+                break;
+            case $eqaul:
+                //
+                break;
+            default:
+                break;
+       }
+    })
+})
